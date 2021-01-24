e00a24921892dfa3e4a0f594c701441271cf4006
sitek94 <msitkowski94@gmail.com>
2021-01-24 17:28:41 +0100

    add root-level request logger middleware

--- app.js
+++ app.js
@@ -3,15 +3,24 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+function requestLoggerMiddleware({ method, path, ip }, res, next) {
+  console.log(`${method} ${path} - ${ip}`);
 
+  next();
+}
+
+// Middlewares
+app.use(express.static(__dirname + '/public'));
+app.use(requestLoggerMiddleware);
+
+// Routes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/json', (req, res) => {
   const message = 'Hello world!';
-  
+
   res.json({
     message:
       process.env.MESSAGE_STYLE === 'uppercase'
