56f70d8d721bb40541d912728462f95a5e5e7bf3
sitek94 <msitkowski94@gmail.com>
2021-01-24 16:50:06 +0100

    serve HTML file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 const PORT = process.env.PORT || 5000;
