4cd45a9f31754967d317926a7607e8b6a2229d95
sitek94 <msitkowski94@gmail.com>
2021-01-24 17:13:54 +0100

    serve JSON on '/json' route

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/json', (req, res) => {
+  const message = 'Hello world!';
+
+  res.json({
+    message,
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
